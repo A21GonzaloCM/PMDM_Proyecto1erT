525a92fc96a27c4f16592c4f5f4b2049a7ae5844
a21gonzalocm <a21gonzalocm@iessanclemente.net>
2023-11-23 14:31:44 +0100

    navbar + boton add

--- UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/MainActivity.kt
+++ UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.uf1_proyecto_compose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposableInferredTarget
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.uf1_proyecto_compose.ui.theme.UF1_Proyecto_composeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                     Greeting("Android")
                 }
             }
@@ -29,12 +44,42 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+    ScaffoldSample()
+   /*     drawerContent = { Text(text = "Drawer Menu 1") },
+        content = { Text("Content") },
+        bottomBar = { BottomAppBar(backgroundColor = MaterialTheme.colors.primary) { Text("Bottom App Bar") } }*/
+}
+
+
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+@ExperimentalMaterial3Api
+fun ScaffoldSample() {
+    Scaffold(
+        topBar = { TopAppBar(title = {Text("ToDoListAPP")  }, Modifier.background(Color.Blue) )},
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = { FloatingActionButton(onClick = {}){
+            Icon(imageVector = Icons.Default.Add, contentDescription = "fab icon")
+
+        }
+        },
+        content = { Text(
+            text="ToDoListAPP",
+            modifier = Modifier.padding(15.dp))
+
+        }
+
+    )
+
 }
 
 @Preview(showBackground = true)
